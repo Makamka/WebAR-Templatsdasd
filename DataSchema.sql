3e53fe093c12dafc085adc5992a5914ac2c9ad1d
Makamka <127287469+Makamka@users.noreply.github.com>
2024-11-21 20:26:46 +0200

    Update DataSchema.sql

--- DataSchema.sql
+++ DataSchema.sql
@@ -1,1 +1,20 @@
+-- Створення таблиці користувачів
+CREATE TABLE Users (
+    UserID INT PRIMARY KEY,                -- Первинний ключ
+    UserName VARCHAR(50) NOT NULL,         -- Ім'я користувача (обов'язковий)
+    Email VARCHAR(100) NOT NULL,           -- Email користувача
+    RegistrationDate DATE DEFAULT CURRENT_DATE, -- Дата реєстрації
 
+    -- Обмеження цілісності для атрибута UserName
+    CONSTRAINT chk_UserName CHECK (UserName ~ '^[A-Za-z]{3,50}$'), 
+    -- Обмеження цілісності для атрибута Email
+    CONSTRAINT chk_Email CHECK (Email ~ '^[\\w.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$')
+);
+
+-- Створення таблиці замовлень
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,                  -- Первинний ключ
+    UserID INT REFERENCES Users(UserID),      -- Зовнішній ключ
+    OrderDate DATE DEFAULT CURRENT_DATE,      -- Дата замовлення
+    OrderTotal DECIMAL(10, 2) NOT NULL        -- Загальна сума замовлення
+);
